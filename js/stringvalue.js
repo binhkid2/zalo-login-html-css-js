481b1beac3192c79aa872488febd97b368904576
LacChinhSoft <128137927+binhkid2@users.noreply.github.com>
2024-06-08 08:20:30 +0000

    verify

--- js/stringvalue.js
+++ js/stringvalue.js
@@ -1,5 +1,6 @@
 const ZALO_PERMISSION_URL = "https://oauth.zaloapp.com/v4/permission"; // Social API
 const ZALO_ACCESS_TOKEN_URL = "https://oauth.zaloapp.com/v4/access_token"; // Social API
 
-const APP_ID = "154765535848006805"; // FILL your Application ID here!!!
+const APP_ID = "4220696386833253137"; // FILL your Application ID here!!!
 const APP_AUTH_URL = window.location.href.substring(0, window.location.href.lastIndexOf("/")) + "/success"; // URL to auth.html
+//https://binhkid2.github.io/zalo-login-html-css-js
